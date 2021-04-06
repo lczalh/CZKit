e50d1ab2e5f474b483181e97205088318ac4e3cd
liuchaozheng <824092805@qq.com>
2021-04-06 17:34:28 +0800

    0.2.0

--- CZKit/UIImageView+Kingfisher/UIImageView+Kingfisher.swift
+++ CZKit/UIImageView+Kingfisher/UIImageView+Kingfisher.swift
@@ -12,7 +12,7 @@ public extension UIImageView {
     
     /// 设置网络图片
     func cz_setImage(url: String?, placeholder: UIImage?, indicatorType: Kingfisher.IndicatorType = .none, options: KingfisherOptionsInfo? = [.transition(.fade(0.8))]) {
-        if  url?.isEmpty == false || url?.hasPrefix("http://") == true || url?.hasPrefix("https://") == true, let url = URL(string: url) {
+        if let string = url, string.isEmpty == false, string.hasPrefix("http://") == true || string.hasPrefix("https://") == true, let url = URL(string: string) {
             kf.indicatorType = indicatorType
             kf.setImage(with: url, placeholder: placeholder, options: options)
         } else {
